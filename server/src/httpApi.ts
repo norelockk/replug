4f6e70e8a8d34d1a7ad01757303cbdfdc54abf39
George T <112294007+norelockk@users.noreply.github.com>
2023-09-15 04:51:48 +0200

    .

---
+++ server/src/httpApi.ts
@@ -0,0 +1,5 @@
+import Replug from "./replug"
+
+export default async function httpApi(replug: Replug) {
+  
+}
--- server/src/replug.ts
+++ server/src/replug.ts
@@ -1,5 +1,13 @@
+import pino from 'pino';
+import avvio from 'avvio';
 import EventEmitter from 'node:events';
 
 export default class Replug extends EventEmitter {
+  private logger: pino.Logger = pino();
 
+  constructor() {
+    super();
+
+    const boot = avvio(this);
+  } 
 }
